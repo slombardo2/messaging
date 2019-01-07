096fdea62a9cdc749e5a178ec79feccee2f70ae9
John Alcorn <jalcorn@us.ibm.com>
2019-01-07 16:34:11 -0500

    Manually create JWT (for now)
    Soon we'll defer to the new Liberty-based OIDC provider "microservice" to get the JWT instead.

--- messaging-ejb/src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/messaging/MessagingMDB.java
+++ messaging-ejb/src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/messaging/MessagingMDB.java
@@ -1,5 +1,5 @@
 /*
-       Copyright 2017 IBM Corp All Rights Reserved
+       Copyright 2017, 2019 IBM Corp All Rights Reserved
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -16,9 +16,14 @@
 
 package com.ibm.hybrid.cloud.sample.stocktrader.messaging;
 
+//Subpackages for this microservice
 import com.ibm.hybrid.cloud.sample.stocktrader.messaging.client.NotificationClient;
 import com.ibm.hybrid.cloud.sample.stocktrader.messaging.json.LoyaltyChange;
 import com.ibm.hybrid.cloud.sample.stocktrader.messaging.json.NotificationResult;
+
+//JSON Web Token (JWT) construction
+import com.ibm.websphere.security.jwt.JwtBuilder;
+import com.ibm.websphere.security.jwt.JwtToken;
 
 //Standard I/O classes
 import java.io.PrintWriter;
@@ -47,9 +52,6 @@ import javax.json.bind.JsonbBuilder;
 //mpRestClient 1.0
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-//mpJWT 1.0
-import org.eclipse.microprofile.jwt.JsonWebToken;
-
 
 /**
  * Message-Driven Bean implementation class for: MessagingMDB
@@ -60,8 +62,6 @@ public class MessagingMDB implements MessageListener {
 	private static Logger logger = Logger.getLogger(MessagingMDB.class.getName());
 
 	private @Inject @RestClient NotificationClient notificationClient;
-
-	private @Inject JsonWebToken jwt;
 
 
 	/**
@@ -76,8 +76,10 @@ public class MessagingMDB implements MessageListener {
 			Jsonb jsonb = JsonbBuilder.create();
 			LoyaltyChange loyaltyChange = jsonb.fromJson(payload, LoyaltyChange.class);
 			String owner = loyaltyChange.getOwner();
+			String id = loyaltyChange.getId();
+			String jwt = createJWT(id); //temporarily create the JWT manually.  soon we'll get from new OIDC microservice instead
 
-			NotificationResult result = notificationClient.notify("Bearer "+jwt.getRawToken(), owner, loyaltyChange);
+			NotificationResult result = notificationClient.notify("Bearer "+jwt, owner, loyaltyChange);
 			logger.info("Received the following response from the Notification microservice: "+result);
 		} catch (Throwable t) {
 			logger.warning("An error occurred processing a JMS message from the queue");
@@ -85,6 +87,42 @@ public class MessagingMDB implements MessageListener {
 		} else {
 			logger.warning("onMessage received a non-TextMessage!");
 		}
+	}
+
+	/**
+	 * Create Json Web Token.
+	 * return: the base64 encoded and signed token. 
+	 */
+	private static String createJWT(String userName) {
+		String jwtTokenString = null;		
+		try {
+			// create() uses default settings.  
+			// For other settings, specify a JWTBuilder element in server.xml
+			// and call create(builder id)
+			JwtBuilder builder = JwtBuilder.create();
+
+			// Put the user info into a JWT Token
+			builder.subject(userName);
+			builder.claim("upn", userName);
+
+			// Set the audience to our sample's value
+			String audience = System.getenv("JWT_AUDIENCE");
+			builder.claim("aud", audience);
+
+			//builder.claim("groups", groups);
+
+			//convention is the issuer is the url, but for demo portability a fixed value is used.
+			//builder.claim("iss", request.getRequestURL().toString());
+			String issuer = System.getenv("JWT_ISSUER");
+			builder.claim("iss", issuer);
+
+			JwtToken theToken = builder.buildJwt();			
+			jwtTokenString = theToken.compact();
+		} catch (Exception e) {			
+			logException(e);
+			throw new RuntimeException(e);
+		}
+		return jwtTokenString;
 	}
 
 	private static void logException(Throwable t) {
